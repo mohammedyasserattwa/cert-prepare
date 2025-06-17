123747a6705237cff7a649c3ffe8ce22a4d106a6
Mohammed Yasser <mohammedyasserattwa@gmail.com>
2025-06-17 12:05:08 +0300

    v1.0 GCP Associate Cloud Engineer

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+import './App.css'
+import { Home } from './pages/home'
+
+function App() {
+
+  return (
+    <div className='min-w-screen'>
+      <Home />
+    </div>
+  )
+}
+
+export default App
