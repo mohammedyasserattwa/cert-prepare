d49e79d186a1f30a998da5760d3909f15ab4552a
Mohammed Yasser <mohammedyasserattwa@gmail.com>
2025-06-21 15:45:47 +0300

    v5.1 Bug Fix

--- src/pages/GCPMLEng.tsx
+++ src/pages/GCPMLEng.tsx
@@ -1,12 +1,12 @@
 import { ExamPrep } from '../components/ExamPrep';
-import GCPDataEngQestions from '../assets/gcp_data_eng_questions.json';
+import GCPMLQuestions from '../assets/gcp_ml_engineer_questions.json';
 import { formatAnswerText, formatQuestionText } from '../helpers/Formatting';
 
 
 export const GCPMLEng = () => {
   return (
     <ExamPrep
-      questionsData={GCPDataEngQestions}
+      questionsData={GCPMLQuestions}
       examTitle="GCP Professional Machine Learning Engineer Exam Prep"
       examDescription="Prepare for your exam with previous exam questions."
       landingDescription="Choose your mode and start practicing!"
